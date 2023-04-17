33b7fe49bfd5d7826801ef157eb7093df5e55396
pwr280 <92118856+pwr280@users.noreply.github.com>
2023-04-17 15:13:52 +0200

    python

--- kakuro.py
+++ kakuro.py
@@ -126,6 +126,21 @@ class grilleKakuro:
             for j in range(self.m):
                 if self.grille[i][j] != -1 and i > 0 and self.grille[i - 1][j] != -1:
                     self.entete[i][j] = (self.entete[i][j][0], 0)
+
+    def getGrille(self):
+        return self.grille
+    
+
+    
+    # def solve(self):
+    #     # backtracking algorithm to solve the grid
+    #     # we start at the top left corner
+    #     # if the cell is empty, we try to fill it with a number
+    #     # if the number is valid, we go to the next cell
+    #     # if the number is not valid, we try another number
+    #     # if we have tried all the numbers and none of them are valid, we go back to the previous cell
+
+    #     while 
     
 test = grilleKakuro(11, 11, 10)
 test.affiche()
