64d4b6007de766340bd044b0068ff58039e31528
Robin Lafontaine <46829268+robinlafontaine@users.noreply.github.com>
2023-04-19 10:14:22 +0200

    Ajout auth.dart & fix build

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-// ...
-
-await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-);
-
 // MAIN
 
-
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
