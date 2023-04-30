38a765d1b4b7e5427f6b01c80ef4e0e58d2fb35f
Robin Lafontaine <46829268+robinlafontaine@users.noreply.github.com>
2023-04-30 17:41:54 +0200

    main optimized

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:audioplayers/audioplayers.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -12,7 +10,6 @@ import 'firebase_options.dart';
 import 'package:kakuro/config/config.dart';
 import 'package:kakuro/config/fonctions.dart';
 import 'auth.dart';
-import 'leaderboard.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -70,7 +67,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
               width: width(context) / 2,
               height: width(context) / 2,
             ),
-            SizedBox(
+            const SizedBox(
               height: 70,
             ),
             boutton(
@@ -80,13 +77,13 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                   if (user == null) {
                     Auth(FirebaseAuth.instance).signInGoogle(context);
                   } else {
-                    config.online=true;
+                    config.online = true;
                     route(context, enligne());
                   }
                 })
               },
             ),
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
             boutton(
