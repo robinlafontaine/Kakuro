c53e38043bb45695bc26ced8304ed449ca98d394
Robin Lafontaine <46829268+robinlafontaine@users.noreply.github.com>
2023-05-10 13:58:24 +0200

    mesparties multi

---
+++ lib/screens/mesparties_multijoueur.dart
@@ -0,0 +1,194 @@
+import 'dart:convert';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:kakuro/config/fonctions.dart';
+import 'package:kakuro/config/config.dart';
+import 'package:kakuro/duels.dart';
+import 'package:kakuro/kakuro.dart';
+import 'package:kakuro/screens/game.dart';
+import 'package:kakuro/screens/multijoueur.dart';
+import 'package:kakuro/screens/parametres.dart';
+import 'package:kakuro/widgets/boutton.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../widgets/appbar.dart';
+import '../widgets/navbar.dart';
+import 'menu.dart';
+
+class mesparties_multijoueur extends StatefulWidget {
+  @override
+  State<mesparties_multijoueur> createState() => _mespartiesState();
+}
+
+class _mespartiesState extends State<mesparties_multijoueur> {
+  List<String> grilles = [];
+  List<String> chronos = [];
+  List<String> minutes = [];
+  List<String> secondes = [];
+  List<String> etats = [];
+  List etatSet = [];
+  List<Kakuro> kakuros = [];
+
+  List setEtat() {
+    List liste = [];
+    for (int k = 0; k < etats.length; k++) {
+      liste.add(jsonDecode(etats[k]));
+    }
+    return liste;
+  }
+
+  String getAffichage(int n) {
+    return n.toString().padLeft(2, "0");
+  }
+
+  Future<bool> launch() async {
+    Future<List> duelsEnCours =
+        Duels.getPendingDuels(FirebaseAuth.instance.currentUser!.uid);
+    print("duelsEnCours: $duelsEnCours");
+    // for (var doc in duelsEnCours) {
+    //   if (doc.data()["etat"] == "attente") {
+    //     route(context, multijoueur());
+    //     return false;
+    //   }
+    // }
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String>? _grilles = await prefs.getStringList("grilles");
+    List<String>? _chronos = await prefs.getStringList("chronos");
+    List<String>? _etats = await prefs.getStringList("etats");
+    _grilles ??= [];
+    _chronos ??= [];
+    _etats ??= [];
+    setState(() {
+      grilles = _grilles!;
+      chronos = _chronos!;
+      etats = _etats!;
+      for (int i = 0; i < grilles.length; i++) {
+        kakuros.add(Kakuro.withXML(grilles[i]));
+        Duration duration = Duration(seconds: int.parse(chronos[i]));
+        minutes.add(getAffichage(duration.inMinutes.remainder(60)));
+        secondes.add(getAffichage(duration.inSeconds.remainder(60)));
+      }
+      etatSet = setEtat();
+    });
+    return grilles.length > 0;
+  }
+
+  void retour() {
+    route(context, menu());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        retour();
+        return true;
+      },
+      child: Scaffold(
+          backgroundColor: config.colors.primaryBackground,
+          appBar: PreferredSize(
+            preferredSize: Size(double.infinity, width(context) / 6),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child:
+                  appbar(home: false, enjeu: false, retour: () => {retour()}),
+            ),
+          ),
+          body: SingleChildScrollView(
+            child: FutureBuilder<bool>(
+                future: launch(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasData) {
+                    if (snapshot.data == true) {
+                      return Center(
+                          child: Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: ListView.builder(
+                            shrinkWrap: true,
+                            scrollDirection: Axis.vertical,
+                            padding: EdgeInsets.all(2),
+                            itemCount: grilles.length,
+                            itemBuilder: (_, i) {
+                              return Container(
+                                margin: EdgeInsets.only(bottom: 20),
+                                padding: EdgeInsets.all(15),
+                                decoration: BoxDecoration(
+                                    color: config.colors.primaryColor),
+                                width: width(context) / 2,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        Text(
+                                            "Taille : (${kakuros[i].n},${kakuros[i].m})",
+                                            style: TextStyle(
+                                                color: config
+                                                    .colors.primaryTextColor)),
+                                        SizedBox(
+                                          height: 3,
+                                        ),
+                                        Text(
+                                            "Difficulté : ${kakuros[i].difficulte}",
+                                            style: TextStyle(
+                                                color: config
+                                                    .colors.primaryTextColor)),
+                                        SizedBox(
+                                          height: 3,
+                                        ),
+                                        Text(
+                                            "Chrono : ${minutes[i]}:${secondes[i]}",
+                                            style: TextStyle(
+                                                color: config
+                                                    .colors.primaryTextColor)),
+                                      ],
+                                    ),
+                                    boutton(
+                                        value: "JOUER",
+                                        couleur: true,
+                                        size: width(context) / 3,
+                                        onPress: () {
+                                          config.newgame = false;
+                                          route(
+                                              context,
+                                              game(
+                                                  kakuro: kakuros[i],
+                                                  base: etatSet[i],
+                                                  index: i,
+                                                  chrono:
+                                                      int.parse(chronos[i])));
+                                        })
+                                  ],
+                                ),
+                              );
+                            }),
+                      ));
+                    } else {
+                      return Center(
+                          child: Text(
+                        'Aucune partie en cours',
+                        style:
+                            TextStyle(color: config.colors.primaryTitreSelect),
+                      ));
+                    }
+                  } else {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                }),
+          ),
+          bottomNavigationBar: navbar(
+              actif: 2,
+              reaload: () {
+                Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => parametre()))
+                    .then((value) {
+                  setState(() {});
+                });
+              })),
+    );
+  }
+}
--- lib/screens/multijoueur.dart
+++ lib/screens/multijoueur.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:kakuro/config/config.dart';
 import 'package:kakuro/screens/classement.dart';
 import 'package:kakuro/screens/invitation.dart';
+import 'package:kakuro/screens/mesparties_multijoueur.dart';
 import 'package:kakuro/screens/nouvellepartie.dart';
 import 'package:kakuro/screens/parametres.dart';
 import 'package:kakuro/widgets/boutton.dart';
@@ -11,78 +12,82 @@ import '../widgets/appbar.dart';
 import '../widgets/navbar.dart';
 import 'menu.dart';
 
-class multijoueur extends StatefulWidget{
-
+class multijoueur extends StatefulWidget {
   @override
   State<multijoueur> createState() => _multijoueurState();
 }
 
 class _multijoueurState extends State<multijoueur> {
-
-  void retour(){
+  void retour() {
     route(context, menu());
   }
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: ()async{
+      onWillPop: () async {
         retour();
         return true;
       },
       child: Scaffold(
-        backgroundColor: config.colors.primaryBackground,
-        appBar: PreferredSize(
-          preferredSize: Size(double.infinity, width(context)/6),
-          child: Padding(
-            padding: const EdgeInsets.all(10),
-            child: appbar(home:false,enjeu:false,retour: ()=>{retour()}),
+          backgroundColor: config.colors.primaryBackground,
+          appBar: PreferredSize(
+            preferredSize: Size(double.infinity, width(context) / 6),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child:
+                  appbar(home: false, enjeu: false, retour: () => {retour()}),
+            ),
           ),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                config.images.icon,
-                width: width(context)/2,
-                height: width(context)/2,
-              ),
-              SizedBox(height: 40,),
-              boutton(
-                  value: "LANCER DUEL",
-                  onPress: (){
-                    route(context, invitation());
-                  }
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              boutton(
-                  value: "MES PARTIES",
-                  onPress: (){}
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              boutton(
-                  value: "RESULTATS",
-                  onPress: (){}
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              boutton(
-                  value: "CLASSEMENT",
-                  onPress: (){route(context, classement());}
-              ),
-            ],
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  config.images.icon,
+                  width: width(context) / 2,
+                  height: width(context) / 2,
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                boutton(
+                    value: "LANCER DUEL",
+                    onPress: () {
+                      route(context, invitation());
+                    }),
+                SizedBox(
+                  height: 10,
+                ),
+                boutton(
+                    value: "MES PARTIES",
+                    onPress: () {
+                      route(context, mesparties_multijoueur());
+                    }),
+                SizedBox(
+                  height: 10,
+                ),
+                boutton(value: "RESULTATS", onPress: () {}),
+                SizedBox(
+                  height: 10,
+                ),
+                boutton(
+                    value: "CLASSEMENT",
+                    onPress: () {
+                      route(context, classement());
+                    }),
+              ],
+            ),
           ),
-        ),
-        bottomNavigationBar: navbar(actif:10,reaload:(){Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => parametre())).then((value) { setState(() {});});
-        })),
+          bottomNavigationBar: navbar(
+              actif: 10,
+              reaload: () {
+                Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => parametre()))
+                    .then((value) {
+                  setState(() {});
+                });
+              })),
     );
   }
 }
