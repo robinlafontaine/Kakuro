358e1a4f33d7d819d2ca231efbf0b8ef0542ea7a
maximezim <maxime.zimmermann57@gmail.com>
2023-05-12 18:50:41 +0200

    transitions

--- lib/config/fonctions.dart
+++ lib/config/fonctions.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 route(context, widget, {bool close = false}) => close
-    ? Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (_) => widget), (route)=> false)
-    : Navigator.of(context).push(MaterialPageRoute(builder: (_) => widget));
+    ? Navigator.of(context).pushAndRemoveUntil(
+        PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) => widget,
+          transitionDuration: const Duration(seconds: 0),
+        ),
+        (route) => false)
+    : Navigator.of(context).push(PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) => widget,
+        transitionDuration: const Duration(seconds: 0),
+      ));
 
-
-
-width(context)=>MediaQuery.of(context).size.width;
-height(context)=>MediaQuery.of(context).size.height;
-
+width(context) => MediaQuery.of(context).size.width;
+height(context) => MediaQuery.of(context).size.height;
